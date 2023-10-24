741576a77416863032ede27c81e2d29e98271f36
Júlio Cesar Urbano Vasconcelos Filho <201711250010@ad.ifpb.edu.br>
2023-10-24 10:40:57 -0300

    insert autor e livro

--- src/br/com/ifpb/livraria/bean/LivroBean.java
+++ src/br/com/ifpb/livraria/bean/LivroBean.java
@@ -3,25 +3,45 @@ package br.com.ifpb.livraria.bean;
 import javax.faces.bean.ManagedBean;
 
 import br.com.ifpb.livraria.dao.DAO;
+import br.com.ifpb.livraria.modelo.Autor;
 import br.com.ifpb.livraria.modelo.Livro;
 
 @ManagedBean
 public class LivroBean {
 
 	private Livro livro = new Livro();
+	
+	private Autor novoAutor = new Autor();
 
 	public Livro getLivro() {
 		return livro;
+	}
+	
+	public Autor getNovoAutor () {
+		return novoAutor;
 	}
 
 	public void gravar() {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
 
+		if (novoAutor == null) {
+			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+		}
+		adicionAutor();
 		if (livro.getAutores().isEmpty()) {
 			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
 		}
-
+		
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
+	}
+
+	private void adicionAutor() {
+		DAO<Autor> daoAutor = new DAO<Autor>(Autor.class);
+		for (Autor autor : daoAutor.listaTodos()) {
+			if (autor.getNome().equals(novoAutor.getNome()) ) {
+				livro.adicionaAutor(autor);
+			}
+		}
 	}
 
 }
